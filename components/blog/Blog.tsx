addf1297b6e6c3bce9812845dede0dca3bf15d0c
Cardona simon <simon.cdn@icloud.com>
2023-03-09 12:17:40 +0100

    ✏️ F01 : Blog component fix typo

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -22,44 +22,47 @@ export default function Blog({ posts }: Props) {
       <div
         className={`${styles.gap} sm:w-[100%] ss:w-[70%] w-[100%] grid sm:grid-cols-2 grid-cols-1 justify-self-center mt-12`}
       >
-        {posts.slice(-2).map((post: any, index: any) => (
-          <Link
-            key={index}
-            href={`/blog/${post.slug.current}`}
-            className="bg-secondary p-2 sm:w-[100%] ss:w-[70%] w-[100%]"
-          >
-            <article>
-              <Image
-                src={urlFor(post.mainImage.asset._ref).url()}
-                alt="article"
-                width={700}
-                height={100}
-              />
+        {posts
+          .slice(-2)
+          .reverse()
+          .map((post: any, index: any) => (
+            <Link
+              key={index}
+              href={`/blog/${post.slug.current}`}
+              className="bg-secondary p-2 sm:w-[100%] ss:w-[70%] w-[100%]"
+            >
+              <article>
+                <Image
+                  src={urlFor(post.mainImage.asset._ref).url()}
+                  alt="article"
+                  width={700}
+                  height={100}
+                />
 
-              <p className="text-gray-500 text-sm pt-2">
-                {new Date(post._createdAt).toLocaleDateString("fr-FR", {
-                  day: "numeric",
-                  month: "long",
-                  year: "numeric",
-                })}{" "}
-                •{" "}
-                <span className="text-tertiary font-semibold">
-                  Workshop by A
-                </span>
-              </p>
-              <h5 className={`${styles.heading5} min-h-[60px]`}>
-                {post.title}
-              </h5>
-              <div className={`${styles.text} description mt-3`}>
-                {post.description}
-              </div>
+                <p className="text-gray-500 text-sm pt-2">
+                  {new Date(post._createdAt).toLocaleDateString("fr-FR", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}{" "}
+                  •{" "}
+                  <span className="text-tertiary font-semibold">
+                    Workshop by A
+                  </span>
+                </p>
+                <h5 className={`${styles.heading5} min-h-[60px]`}>
+                  {post.title}
+                </h5>
+                <div className={`${styles.text} description mt-3`}>
+                  {post.description}
+                </div>
 
-              <button className="px-2 py-1 bg-tertiary rounded-sm text-white mt-3">
-                En voir plus
-              </button>
-            </article>
-          </Link>
-        ))}
+                <button className="px-2 py-1 bg-tertiary rounded-sm text-white mt-3">
+                  En voir plus
+                </button>
+              </article>
+            </Link>
+          ))}
       </div>
 
       <Button label="Voir tout les articles" route="/blog" />
