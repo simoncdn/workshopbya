064deccad049ab6bcf949ff1519c6d3e226663f8
Cardona simon <simon.cdn@icloud.com>
2023-03-09 17:35:20 +0100

    💄 F02 : RICKTEXT Component > add style

--- components/reusable-ui/RichTextComponents.tsx
+++ components/reusable-ui/RichTextComponents.tsx
@@ -1,14 +1,13 @@
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Link from "next/link";
 import { urlFor } from "@/lib/sanity.client";
+import styles from "@/styles";
 
 export const RichTextComponents = {
   types: {
     image: ({ value }: any) => {
       return (
-        <div className="relative w-full h-96 m-10 mx-auto">
+        <div className="relative w-full h-96 mx-auto">
           <Image
             className="object-contain"
             src={urlFor(value).url()}
@@ -20,20 +19,26 @@ export const RichTextComponents = {
     },
   },
   block: {
-    h1: ({ children }: any) => (
-      <h1 className="text-5xl py-10 font-bold text-primary">{children}</h1>
-    ),
     h2: ({ children }: any) => (
-      <h2 className="text-4xl py-10 font-bold text-primary">{children}</h2>
+      <h2 className="md:text-4xl sm:text-3xl text-2xl py-10 font-bold text-primary font-text">
+        {children}
+      </h2>
     ),
     h3: ({ children }: any) => (
-      <h3 className="text-3xl py-10 font-bold text-primary">{children}</h3>
+      <h3 className="md:text-3xl sm:text-2xl text-xl py-10 font-bold text-primary font-text">
+        {children}
+      </h3>
     ),
     h4: ({ children }: any) => (
-      <h4 className="text-2xl py-10 font-bold text-primary">{children}</h4>
+      <h4 className="md:text-2xl sm:text-xl text-lg py-10 font-bold text-primary font-text">
+        {children}
+      </h4>
     ),
+    p: ({ children }: any) => <p className={`${styles.text}`}>{children}</p>,
     blockquote: ({ children }: any) => (
-      <blockquote className="border-l-tertiary border-l-4 pl-5 py-5 my-5">
+      <blockquote
+        className={`${styles.text} border-l-tertiary border-l-4 pl-5 py-5 my-5`}
+      >
         {children}
       </blockquote>
     ),
@@ -41,12 +46,12 @@ export const RichTextComponents = {
 
   list: {
     bullet: ({ children }: any) => (
-      <ul className="ml-10 py-5 space-y-5 list-disc text-primary">
+      <ul className={`${styles.text} ml-10 py-5 list-disc space-y-5 w-[80%]`}>
         {children}
       </ul>
     ),
     number: ({ children }: any) => (
-      <ul className="mt-lg list-decimal">{children}</ul>
+      <ul className={`${styles.text} mt-lg list-decimal`}>{children}</ul>
     ),
   },
   marks: {
