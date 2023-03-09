53ab2d97699e301592cdca3d398d7b22db6150bb
Cardona simon <simon.cdn@icloud.com>
2023-03-09 11:57:46 +0100

    🐛 F01 : vercel run build failed fixed

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -4,12 +4,14 @@ import  ImageUrlBuilder  from "@sanity/image-url";
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
+const token = process.env.NEXT_PUBLIC_SANITY_TOKENNEXT_PUBLIC_SANITY_TOKEN
 
 export const client = createClient({
     projectId,
     dataset,
     apiVersion,
     useCdn: true,
+    token,
 })
 
 const builder = ImageUrlBuilder(client);
