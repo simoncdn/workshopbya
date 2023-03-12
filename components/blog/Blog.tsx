150bc9f38db76c36c6725d2f1c6bc65d1b99e9c9
Cardona simon <simon.cdn@icloud.com>
2023-03-13 00:45:02 +0100

    💫 F02 : Add blog animation

--- components/blog/Blog.tsx
+++ components/blog/Blog.tsx
@@ -5,14 +5,14 @@ import Link from "next/link";
 import Button from "../reusable-ui/Button";
 import SectionTitle from "../reusable-ui/SectionTitle";
 import { client, urlFor } from "@/lib/sanity.client";
+import { motion } from "framer-motion";
+import { sectionAnimate } from "@/animation/animation";
 
 type Props = {
   posts: Post[];
 };
 
 export default function Blog({ posts }: Props) {
-  console.log(posts);
-
   return (
     <section
       className={`${styles.paddingX} ${styles.flexCol} relative sm:items-start items-center my-[100px]`}
@@ -26,42 +26,47 @@ export default function Blog({ posts }: Props) {
           .slice(-2)
           .reverse()
           .map((post: any, index: any) => (
-            <Link
+            <motion.li
+              initial={"initial"}
+              whileInView={"onScreen"}
+              variants={sectionAnimate}
+              viewport={{ once: true, amount: 0.3 }}
               key={index}
-              href={`/blog/${post.slug.current}`}
-              className="bg-secondary p-2 sm:w-[100%] ss:w-[70%] w-[100%]"
+              className="list-none bg-secondary p-2 sm:w-[100%] ss:w-[70%] w-[100%]"
             >
-              <article>
-                <Image
-                  src={urlFor(post.mainImage.asset._ref).url()}
-                  alt="article"
-                  width={700}
-                  height={100}
-                />
+              <Link href={`/blog/${post.slug.current}`}>
+                <article>
+                  <Image
+                    src={urlFor(post.mainImage.asset._ref).url()}
+                    alt="article"
+                    width={700}
+                    height={100}
+                  />
 
-                <p className="text-gray-500 text-sm pt-2">
-                  {new Date(post._createdAt).toLocaleDateString("fr-FR", {
-                    day: "numeric",
-                    month: "long",
-                    year: "numeric",
-                  })}{" "}
-                  •{" "}
-                  <span className="text-tertiary font-semibold">
-                    Workshop by A
-                  </span>
-                </p>
-                <h5 className={`${styles.heading5} min-h-[60px]`}>
-                  {post.title}
-                </h5>
-                <div className={`${styles.text} description mt-3`}>
-                  {post.description}
-                </div>
+                  <p className="text-gray-500 text-sm pt-2">
+                    {new Date(post._createdAt).toLocaleDateString("fr-FR", {
+                      day: "numeric",
+                      month: "long",
+                      year: "numeric",
+                    })}{" "}
+                    •{" "}
+                    <span className="text-tertiary font-semibold">
+                      Workshop by A
+                    </span>
+                  </p>
+                  <h5 className={`${styles.heading5} min-h-[60px]`}>
+                    {post.title}
+                  </h5>
+                  <div className={`${styles.text} description mt-3`}>
+                    {post.description}
+                  </div>
 
-                <button className="px-2 py-1 bg-tertiary rounded-sm text-white mt-3">
-                  En voir plus
-                </button>
-              </article>
-            </Link>
+                  <button className="px-2 py-1 bg-tertiary rounded-sm text-white mt-3">
+                    En voir plus
+                  </button>
+                </article>
+              </Link>
+            </motion.li>
           ))}
       </div>
 
