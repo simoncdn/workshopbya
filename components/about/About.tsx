55ac106c328f22053c3ddb1c84227a9377e27ee7
Cardona simon <simon.cdn@icloud.com>
2023-03-13 00:42:45 +0100

    💫 F02 : Add about component animation

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -5,38 +5,51 @@ import styles from "@/styles";
 import Button from "../reusable-ui/Button";
 import SectionTitle from "../reusable-ui/SectionTitle";
 import aboutpic from "../../assets/WorkshopbyA-About.jpg";
+import { motion } from "framer-motion";
+import { sectionAnimate } from "@/animation/animation";
 
 export default function About() {
   return (
     <section
       className={`${styles.paddingX} ${styles.paddingY} ${styles.gap} relative flex sm:flex-row flex-col-reverse items-center`}
     >
-      <div className="absolute sm:w-[30%] w-[55%] left-0 sm:top-[0%] ss:top-[40%] top-[40%]">
+      <div className="lg:hidden block absolute sm:w-[30%] w-[55%] left-0 sm:top-[0%] ss:top-[40%] top-[40%]">
         <Image src={ellipse} alt="ellipse" />
       </div>
 
       {/*LEFT PART*/}
-      <div className="sm:w-6/12 relative w-full flex justify-center sm:mt-0 ">
+      <motion.div
+        initial={"initial"}
+        whileInView={"onScreen"}
+        variants={sectionAnimate}
+        viewport={{ once: true, amount: 0.3 }}
+        className="sm:w-6/12 relative w-full flex justify-center sm:mt-0 "
+      >
         <Image
           src={aboutpic}
           alt="alizeeplazen"
           className="relative sm:w-[100%] ss:w-[70%] z-[100%]  rounded-t-full"
         />
-      </div>
-
+      </motion.div>
       {/*RIGHT PART*/}
       <div
         className={`${styles.flexCol} sm:items-start items-center sm:w-6/12 relative`}
       >
         <SectionTitle title="À propos" id="about" />
 
-        <p className={`${styles.paragraph} mt-10`}>
+        <motion.p
+          initial={"initial"}
+          whileInView={"onScreen"}
+          variants={sectionAnimate}
+          viewport={{ once: true, amount: 0.3 }}
+          className={`${styles.paragraph} mt-10`}
+        >
           Bonjour, je m'appelle Alizée et je suis une jeune entrepreneure
           passionnée par la communication digitale. Tout a commencé lorsque j'ai
           réalisé des études dans le domaine de la coiffure à l'âge de 14 ans.
           Bien que j'ai aimé ce métier, j'ai rapidement compris que ma véritable
           passion résidait ailleurs...
-        </p>
+        </motion.p>
 
         <Button label="En savoir plus" route="/about" />
       </div>
