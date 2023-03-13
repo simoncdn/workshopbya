c508abcd53074e920740b08bf774541dd165870b
Cardona simon <simon.cdn@icloud.com>
2023-03-13 01:30:26 +0100

    ✏️ F03 : LAYER > fix items-center

--- components/layer/Layer.tsx
+++ components/layer/Layer.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 
 export default function Layer({ children }: any) {
   return (
-    <div className={`${styles.flexCol} min-h-[100vh] h-[100%]`}>
+    <div className={`${styles.flexCol} items-center min-h-[100vh] h-[100%]`}>
       <Header />
       <div className={`${styles.boxWidth} ${styles.flexCenter} flex-1 `}>
         {children}
